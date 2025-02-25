19e9b6649e7c746f1f01b9fe49fa5d63fe86662e
truew1n <andreakowalski12@gmail.com>
2025-02-25 04:17:33 +0100

    Engine <> Game - Setup

---
+++ Engine/VertexBuffer.h
@@ -0,0 +1,14 @@
+#ifndef EN_VERTEX_BUFFER_H
+#define EN_VERTEX_BUFFER_H
+
+#include "assimp/Importer.hpp"
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+class CVertexBuffer {
+public:
+	aiColor3D asd;
+};
+
+#endif
+
